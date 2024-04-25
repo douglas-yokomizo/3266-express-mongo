475a2b04dd32aae24f42fdc3e61710aa0cd0fcd1
Douglas <yogi.yokomizo@gmail.com>
2024-04-25 07:39:06 -0300

    refactor: add dotenv package and configure environment variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dbConnect from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const connection = await dbConnect();
 
@@ -14,16 +15,13 @@ connection.once("open", () => {
 const app = express();
 app.use(express.json());
 
-const buscarLivroPorId = (id) => {
-  return livros.findIndex((livro) => livro.id === +id);
-};
-
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-  mongoose.connect(
-    "mongodb+srv://admin:admin@cluster0.iu3plxi.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
 
   return mongoose.connection;
 };
